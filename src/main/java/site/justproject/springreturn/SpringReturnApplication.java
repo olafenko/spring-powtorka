bd8e638d0e3ce0317b221aae62e0524d3da0a7b9
olafenko <olopikolo666@gmail.com>
2025-07-25 20:40:30 +0200

    init commit

---
+++ src/main/java/site/justproject/springreturn/SpringReturnApplication.java
@@ -0,0 +1,13 @@
+package site.justproject.springreturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReturnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReturnApplication.class, args);
+    }
+
+}
