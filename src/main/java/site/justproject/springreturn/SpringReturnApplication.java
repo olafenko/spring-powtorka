3c2db0ec0ade651f88d4f52b767e8ae70b877d06
olafenko <olopikolo666@gmail.com>
2025-07-25 21:57:22 +0200

    testowane nie przez debugger tylko przez metode oznaczoną adnotacja EventListener i jako parametr event odpowiadający włączeniu aplikacji, oraz autowiring przez konstruktor i manipulowanie wybranym beanem umbrelli przez nazwe podawaną w konstruktorze(pole w klasie Worker może miec dowolną nazwe)

--- src/main/java/site/justproject/springreturn/SpringReturnApplication.java
+++ src/main/java/site/justproject/springreturn/SpringReturnApplication.java
@@ -1,13 +1,25 @@
 package site.justproject.springreturn;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringReturnApplication {
 
+    @Autowired
+    private Worker worker;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringReturnApplication.class, args);
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void start(){
+
+        worker.work();
     }
 
 }
--- src/main/java/site/justproject/springreturn/Worker.java
+++ src/main/java/site/justproject/springreturn/Worker.java
@@ -1,22 +1,21 @@
 package site.justproject.springreturn;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import site.justproject.springreturn.model.Umbrella;
 
 @Component
 public class Worker {
 
-    @Autowired
-    private Umbrella umbrella2;
+
+    private Umbrella parasol;
 
 
     public Worker(Umbrella umbrella2) {
-        this.umbrella2 = umbrella2;
+        this.parasol = umbrella2;
     }
 
     public void work(){
-        System.out.println("Working with: " + umbrella2.getName());
+        System.out.println("Working with: " + parasol.getName());
     }
 
 
