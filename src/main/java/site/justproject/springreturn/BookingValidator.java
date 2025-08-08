e00d50760afba03317be28f9deaddfd276771091
olafenko <olopikolo666@gmail.com>
2025-08-08 18:10:32 +0200

    added 2 simple tests for BookingValidator class, that test success case, and failed case when incorrect date were passed

--- src/main/java/site/justproject/springreturn/BookingValidator.java
+++ src/main/java/site/justproject/springreturn/BookingValidator.java
@@ -1,12 +1,22 @@
 package site.justproject.springreturn;
 
+import java.time.LocalDateTime;
+
 public class BookingValidator {
 
 
     public String validateBooking(BookingRequest request){
 
-       return "Success";
+        return validateBookDate(request);
 
+    }
+
+    private static String validateBookDate(BookingRequest request) {
+        if(request.date().isBefore(LocalDateTime.now())){
+            return "Failed";
+        }
+
+        return "Success";
     }
 
 }
--- src/test/java/site/justproject/springreturn/BookingValidatorTest.java
+++ src/test/java/site/justproject/springreturn/BookingValidatorTest.java
@@ -2,11 +2,48 @@ package site.justproject.springreturn;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.print.Book;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookingValidatorTest {
 
     @Test
-    void validateBooking() {
+    void should_return_success_validation_when_user_gave_correct_request() {
+
+        //given
+        BookingValidator testValidator = new BookingValidator();
+
+        LocalDateTime bookDate = LocalDateTime.of(2025, Month.AUGUST,8,19,30);
+        BookingRequest testRequest = new BookingRequest("Sucharskiego", "Konarskiego", "Ropczyce",bookDate);
+
+        //when
+        String result = testValidator.validateBooking(testRequest);
+
+        //then
+        assertThat(result).isEqualTo("Success");
+
     }
+
+    @Test
+    void should_return_failed_validation_when_user_gave_incorrect_date() {
+
+        //given
+        BookingValidator testValidator = new BookingValidator();
+
+        LocalDateTime bookDate = LocalDateTime.of(2025, Month.AUGUST,8,15,30);
+        BookingRequest testRequest = new BookingRequest("Sucharskiego", "Konarskiego", "Ropczyce",bookDate);
+
+        //when
+        String result = testValidator.validateBooking(testRequest);
+
+        //then
+        assertThat(result).isEqualTo("Failed");
+
+    }
+
+
 }
